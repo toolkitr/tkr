fccef68b043d9828b7fc06b9ef11ae8d5c209ea3
toolkitr <135540404+toolkitr@users.noreply.github.com>
2023-11-18 03:10:58 -0500

    Update cli.py

--- tkr/cli.py
+++ tkr/cli.py
@@ -2,5 +2,18 @@ __all__: tuple = (
   'main',
 )
 
-def main(*args, **kwargs) -> str:
-  return '>>> tkr_help_hook()'
+import json
+import pathlib
+
+def main(*args) -> None:
+    path_abs: str = f'{pathlib.Path(__file__).parent.absolute()}/core/coreattrs.json'
+
+    with open(path_abs, 'r') as file:
+      data = json.load(file)
+
+    data['1  `1  '] = 'value'
+
+    with open(path_abs, 'w') as file:
+      json.dump(data, file, indent=4)
+if __name__ == '__main__':
+    main()
